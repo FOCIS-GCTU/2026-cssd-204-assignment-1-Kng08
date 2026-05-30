780395ee1a0ed56ab880a0297888583fc0164d7e
Kingsley Foli Kaine <2425404154@live.gctu.edu.gh>
2026-05-30 22:30:18 +0000

    Implement initials printing in main function
    Added a function to print initials and formatted output.

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
+#File: KFA.py
+#Description: Print Out My Initials
+#Assignment Number: 1
+#
+#Name: Kingsley Foli Amarkwei
+#Student ID: 2425404154
+#Email: 2425404154@live.gctu.edu.gh
+#Grader: Kingsley
+#
+#On my honor, Kingsley Foli Amarkwei, this programming assignment is my own work
+#and I have not provided this code to ant other student.
 
-def main():
-  pass #code goes here
+def main () :
+    print()
+    print("...KFA")
+    print()
 
-
+    print("...KKKK....KKKK........FFFFFFFFFFFF...........AAAA.........")
+    print("...KKKK...KKKK.........FFFFFFFFFFFF...........AAAA.........")
+    print("...KKKK..KKKK..........FFFFF.................AA..AA........")
+    print("...KKKK.KKKK...........FFFFFFFFFFFF..........AA..AA........")
+    print("...KKKKKKKK............FFFFFFFFFFFF.........AAAAAAAA.......")
+    print("...KKKKKKKKK...........FFFFF................AAAAAAAA.......")
+    print("...KKKKK..KKK..........FFFFF...............AAA....AAA......")
+    print("...KKKK...KKKK....**...FFFFF..........**...AAA....AAA....**")
+    print("...KKKK....KKKK...**...FFFFF..........**...AAAA...AAAA...**")
 
 main()
